50d6134a0bb798c638c04c910adf1710fcd5c929
ajwhite <contact@atticuswhite.com>
2020-03-26 05:08:17 +0000

    Deploying to gh-pages from master fab84d0396e0404bc7107ccbdfd420f4dc13430a

---
+++ precache-manifest.129ff77722835422afdb6dcf1c50ee3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34700fb0b97ad1fc7520aa964a1a239c",
+    "url": "https://howmanypeoplearesick.com/index.html"
+  },
+  {
+    "revision": "834348818966ecf5ef07",
+    "url": "https://howmanypeoplearesick.com/static/css/main.e8c54ef2.chunk.css"
+  },
+  {
+    "revision": "eeec24995cf0bf8b0f6d",
+    "url": "https://howmanypeoplearesick.com/static/js/2.73402e6b.chunk.js"
+  },
+  {
+    "revision": "737c637c7902752c5b98905ed1dd3b22",
+    "url": "https://howmanypeoplearesick.com/static/js/2.73402e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834348818966ecf5ef07",
+    "url": "https://howmanypeoplearesick.com/static/js/main.4b3e149a.chunk.js"
+  },
+  {
+    "revision": "d444b1c47ea42c2cb237",
+    "url": "https://howmanypeoplearesick.com/static/js/runtime-main.446efb5d.js"
+  },
+  {
+    "revision": "29d5df28bae6cd3850aa1a2134b220a4",
+    "url": "https://howmanypeoplearesick.com/static/media/background.29d5df28.png"
+  },
+  {
+    "revision": "c60e4f07b2bc1869ead965f38279499a",
+    "url": "https://howmanypeoplearesick.com/static/media/bing.c60e4f07.png"
+  },
+  {
+    "revision": "cb419ba7dfee5f6d7139486c645612eb",
+    "url": "https://howmanypeoplearesick.com/static/media/informationisbeautiful.cb419ba7.png"
+  },
+  {
+    "revision": "60810e2b77973022c58d2178665c33c1",
+    "url": "https://howmanypeoplearesick.com/static/media/jhu.60810e2b.png"
+  },
+  {
+    "revision": "e1ff104e1deae6d25dd57ed8dbc0f913",
+    "url": "https://howmanypeoplearesick.com/static/media/ncov2019.e1ff104e.png"
+  }
+]);
